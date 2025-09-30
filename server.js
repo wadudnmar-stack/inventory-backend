8aa6756e312588e71516e191920115a2dc4a1ac3
wadudnmar-stack <wadudnmar@gmail.com>
2025-10-01 01:54:16 +0300

    fix console log syntax

--- server.js
+++ server.js
@@ -61,5 +61,5 @@ app.get("*", (req, res) => {
 
 // Start server
 app.listen(PORT, () => {
-  console.log(🚀 Server is running on port ${PORT});
+console.log(`🚀 Server is running on port ${PORT}`);
 });
